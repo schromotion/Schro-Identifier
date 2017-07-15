d3792655242164ce8da6d91d3080ffff41162c2d
k.c. schroeter <kesc0188@colorado.edu>
2017-07-14 23:50:43 -0400

    Add unknown picture test

--- face_id.py
+++ face_id.py
@@ -1,5 +1,6 @@
 import face_recognition
 
+# Add these into array
 picKellen = face_recognition.load_image_file("kellen.jpg")
 picEllen = face_recognition.load_image_file("ellen.jpg")
 picMackie = face_recognition.load_image_file("mackie.jpg")
@@ -11,3 +12,10 @@ encodeEllen = face_recognition.face_encodings(picEllen)[0]
 encodeMackie = face_recognition.face_encodings(picMackie)[0]
 encodeDan = face_recognition.face_encodings(picDan)[0]
 encodeTara = face_recognition.face_encodings(picTara)[0]
+
+unknown_picture = face_recognition.load_image_file("unknown.jpg")
+unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
+
+
+#Run through Array
+results = face_recognition.compare_faces([family[pic]])
