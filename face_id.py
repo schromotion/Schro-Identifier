9410f3b2271d3696eb48af121a9afb90de696c9c
k.c. schroeter <kesc0188@colorado.edu>
2017-07-14 23:59:35 -0400

    Add family array

--- face_id.py
+++ face_id.py
@@ -1,6 +1,5 @@
 import face_recognition
 
-# Add these into array
 picKellen = face_recognition.load_image_file("kellen.jpg")
 picEllen = face_recognition.load_image_file("ellen.jpg")
 picMackie = face_recognition.load_image_file("mackie.jpg")
@@ -13,9 +12,15 @@ encodeMackie = face_recognition.face_encodings(picMackie)[0]
 encodeDan = face_recognition.face_encodings(picDan)[0]
 encodeTara = face_recognition.face_encodings(picTara)[0]
 
+family = [encodeKellen, encodeEllen, encodeMackie, encodeDan, encodeTara]
+
 unknown_picture = face_recognition.load_image_file("unknown.jpg")
 unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
 
 
-#Run through Array
-results = face_recognition.compare_faces([family[pic]])
+for members in family:
+    result = face_recognition.compare_faces([family[member]], unknown_face_encoding)
+
+    if results[0] == True:
+        print("It's a picture of")
+        
